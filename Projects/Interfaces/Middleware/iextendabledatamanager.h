a8b8a5adaad4e297adf5ccf05d9b86ef3dc689da
Kirill Krylov <c4rb0n_unit@protonmail.com>
2020-06-07 19:52:37 +0300

    Update data manager interface

--- Projects/Interfaces/Middleware/iextendabledatamanager.h
+++ Projects/Interfaces/Middleware/iextendabledatamanager.h
@@ -21,17 +21,9 @@ class QAbstractItemModel;
 class IExtendableDataManager
 {
 public:
-    struct ManagerDataItem
-    {
-        int id;
-        QMap<QString, QVector<QVariant> > dataChunks;
-    };
-
     virtual bool RegisterExtentionFieldEditor(QString relation, QString field, QWidget *widget) = 0;
     virtual QWidget *GetExtentionFieldEditor(QString relation, QString field) = 0;
 
-    virtual QList<ManagerDataItem> GetDataList(QString treeName) = 0;
-    virtual ManagerDataItem GetDataItem(QString treeName, int id) = 0;
     virtual QMap<QString, QVariant::Type> GetTableHeader(QString treeName) = 0;
 
     virtual QPointer<QAbstractItemModel> GetDataModel(QString treeName) = 0;
@@ -41,11 +33,6 @@ public:
                               QMap<QString, QVariant::Type> fields,
                               QVector<QVariant> defaultData) = 0;
     virtual bool DeleteExtention(QString mainName, QString relationName) = 0;
-    virtual bool SetActiveExtention(QString mainName, QString relationName) = 0;
-
-    virtual int AddItem(QString treeName, ManagerDataItem task) = 0;
-    virtual bool UpdateItem(QString treeName, ManagerDataItem task) = 0;
-    virtual bool DeleteItem(QString treeName, int id) = 0;
 };
 Q_DECLARE_INTERFACE(IExtendableDataManager, "IExtendableDataManager/1.0")
 //! \}
